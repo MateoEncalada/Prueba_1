8e33e2d3328cd99c69b15cf33946ef278df57065
Mateo Encalada <mateo.encalada@udla.edu.ec>
2023-05-09 20:34:34 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello hola mundo que tal todo!");
+    }
+}
---
+++ src/prueba.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class prueba {
+    private JPasswordField passwordField1;
+}
